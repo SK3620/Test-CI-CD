f1028a194bd8f261ef2e679fabe82527ec31b9d9
SK3620 <k.n.t11193620@gmail.com>
2025-05-13 18:35:33 +0900

    update: サンプルテストコードを調整

--- TestCICDTests/TestCICDTests.swift
+++ TestCICDTests/TestCICDTests.swift
@@ -10,7 +10,7 @@ import Testing
 struct TestCICDTests {
 
     @Test func example() async throws {
-        #expect(3 + 4 == 7)
+        #expect(3 + 6 == 9)
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
 }
