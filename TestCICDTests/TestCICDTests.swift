3abd848c2086f5ac063a14c7559154cabd07eebc
SK3620 <k.n.t11193620@gmail.com>
2025-05-13 14:19:16 +0900

    add: Develop, Staging, Production 開発環境分け

--- TestCICDTests/TestCICDTests.swift
+++ TestCICDTests/TestCICDTests.swift
@@ -6,11 +6,12 @@
 //
 
 import Testing
-@testable import TestCICD
+import TestCICD
 
 struct TestCICDTests {
 
     @Test func example() async throws {
+        #expect(true)
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
 
