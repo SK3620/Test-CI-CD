090695c4992b88500e709f83525d159681ba6344
SK3620 <k.n.t11193620@gmail.com>
2025-05-13 19:24:49 +0900

    update:

--- TestCICDTests/TestCICDTests.swift
+++ TestCICDTests/TestCICDTests.swift
@@ -10,7 +10,7 @@ import Testing
 struct TestCICDTests {
 
     @Test func example() async throws {
-        #expect(0 + 23 == 23)
+        #expect(0 + 2 == 2)
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
 }
